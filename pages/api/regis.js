79cde1b273c544ab9902b733e58ff0242152149f
Somsak1971 <somsak.ve@gmail.com>
2022-07-26 19:22:25 +0700

    last commit

--- pages/api/regis.js
+++ pages/api/regis.js
@@ -9,7 +9,7 @@ export default async function handler (req,res){
   try {
 
     const regisuser = await Regisuser.create(req.body);
-    res.redirect('/')
+    res.redirect('/Home')
     if(!regisuser){
         return res.json({code:'User not created'})
     }
